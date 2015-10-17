1ecdb68b0347c8e15ab09f4beceda282cccae89f
Steve Richey <srichey@floatlearning.com>
2015-10-17 16:48:45 -0500

    add GLCameraView to activity

--- app/src/main/java/com/floatlearning/fl3dtest/MainActivity.java
+++ app/src/main/java/com/floatlearning/fl3dtest/MainActivity.java
@@ -2,12 +2,18 @@ package com.floatlearning.fl3dtest;
 
 import android.app.Activity;
 
+import com.floatlearning.glcameraview.GLCameraView;
+
 public class MainActivity extends Activity {
+    private GLCameraView cameraView;
+
     @Override
     protected void onResume() {
         super.onResume();
 
         // create a simple FL3D scene
+        cameraView = new GLCameraView(this);
+        setContentView(cameraView);
     }
 
     @Override
@@ -15,5 +21,6 @@ public class MainActivity extends Activity {
         super.onPause();
 
         // tear down our scene
+        cameraView.dispose();
     }
 }
